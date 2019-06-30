79e2828de322569e915c538123543bf99d3f4429
unknown <edwardnatividad@gmail.com>
2019-06-30 12:55:01 -0400

    corrected mobile navigation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
 import { AuthService } from './auth.service';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+
   constructor(public auth: AuthService) {
     auth.handleAuthentication();
   }
+
+
 }
